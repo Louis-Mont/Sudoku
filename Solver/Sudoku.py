463ec554a3c44f6ba3bcca9090c6d7a3593bb35d
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-22 17:05:14 +0200

    Added a solve running numerous time until the sudoku is solved/ unsolvable as of yet

--- Solver/Sudoku.py
+++ Solver/Sudoku.py
@@ -12,29 +12,43 @@ class Sudoku:
         """
         self.base = base
         self.sols = {}
-        for x, r in enumerate(base):
-            for y in range(len(r)):
-                s = self.h_sols(x).intersection(self.v_sols(y)).intersection(self.sq_sol(x, y))
-                if len(s) == 1:
-                    base[x, y] = list(s)[0]
-                else:
-                    self.sols[x, y] = s
 
     def h_sols(self, x):
-        all_sol = {i for i in range(1, 10)}
+        all_sol = set(range(1, 10))
         for y in self.base[x]:
             tryremove(all_sol, y)
         return all_sol
 
     def v_sols(self, y):
-        all_sol = {i for i in range(1, 10)}
+        all_sol = set(range(1, 10))
         for x in self.base[:, y]:
             tryremove(all_sol, x)
         return all_sol
 
-    def sq_sol(self, x, y):
-        all_sol = {i for i in range(1, 10)}
+    def sq_sols(self, x, y):
+        all_sol = set(range(1, 10))
         for sq_x in self.base[sq_o(x):sq_o(x) + 3, sq_o(y): sq_o(y) + 3]:
             for sq_y in sq_x:
                 tryremove(all_sol, sq_y)
         return all_sol
+
+    def solve(self):
+        solved = False
+        while not solved:
+            blanks = np.count_nonzero(self.base == 0)
+            solved = True
+            for x, r in enumerate(self.base):
+                for y in range(len(r)):
+                    if self.base[x, y] == 0:
+                        solved = False
+                        s = self.h_sols(x).intersection(self.v_sols(y)).intersection(self.sq_sols(x, y))
+                        if len(s) == 1:
+                            self.base[x, y] = list(s)[0]
+                            try:
+                                self.sols.pop((x, y))
+                            except KeyError:
+                                pass
+                        else:
+                            self.sols[x, y] = s
+            if blanks == np.count_nonzero(self.base == 0):
+                return
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from Solver.Sudoku import Sudoku
 
 if __name__ == '__main__':
     p = [random.sample(range(10), 9) for j in range(0, 9)]
-    print(p)
+    # print(p)
     arr = np.array(p)
     print(arr)
     sv = Sudoku(arr)
+    sv.solve()
     print(sv.base)
