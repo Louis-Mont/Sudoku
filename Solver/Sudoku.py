08602c193f09cdc8569cfbafdb4020592821224d
ER1 <erbreiz95@gmail.com>
2021-06-22 18:18:28 +0200

    Generation sudoku complet et valide (troué)

--- Solver/Sudoku.py
+++ Solver/Sudoku.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import ndarray
-
+import random
 
 class Sudoku:
 
@@ -52,3 +52,26 @@ class Sudoku:
             # TODO : Not Good Sudoku
             pass
         return l
+
+    def generateCompleteSudoku(self):
+        x = 0
+        while x < len(self.base):
+            for y in range(len(self.base[x])):
+                try:
+                    cSol = self.h_sols(x).intersection(self.v_sols(y)).intersection(self.sq_sol(x, y))
+                    pos = random.randint(0, len(cSol) - 1)
+                    self.base[x, y] = list(cSol)[pos]
+                except:
+                    self.base[x] = [0 for i in range(9)]
+                    x -= 1
+                    break
+            x += 1
+
+    def drillSudoku(self, c):
+        i = 0
+        while i < c:
+            x = random.randint(0, len(self.base) - 1)
+            y = random.randint(0, len(self.base[x]) - 1)
+            if self.base[x,y] > 0:
+                self.base[x,y] = 0
+                i += 1
--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import random
 from Solver.Sudoku import Sudoku
 
 if __name__ == '__main__':
-    p = [random.sample(range(10), 9) for j in range(0, 9)]
-    print(p)
-    arr = np.array(p)
-    print(arr)
-    sv = Sudoku(arr)
-    print(sv.base)
+    grid = [[0 for i in range(9)] for j in range(0, 9)]
+
+    arr = np.array(grid)
+    sdk = Sudoku(arr)
+
+    sdk.generateCompleteSudoku()
+    #print(sdk.base)
+
+    sdk.drillSudoku(40)
+    print(sdk.base)
+
+    
