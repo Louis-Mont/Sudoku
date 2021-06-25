984b6cc51cfa9225ddb511c3ee4a6f56224a3449
VMY <vincent.my@hotmail.fr>
2021-06-25 10:43:40 +0200

    Update

---
+++ main.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+tab = [[5,3,0,0,7,0,0,0,0],
+       [6,0,0,1,9,5,0,0,0],
+       [0,9,8,0,0,0,0,6,0],
+       [8,0,0,0,6,0,0,0,3],
+       [4,0,0,8,0,3,0,0,1],
+       [7,0,0,0,2,0,0,0,6],
+       [0,6,0,0,0,0,2,8,0],
+       [0,0,0,4,1,9,0,0,5],
+       [0,0,0,0,8,0,0,0,0]]
+
+def possible(row, column, number):
+    # Vérification si le nombre apparait bien dans la ligne
+    for i in range(0,9):
+        if tab[row][i] == number:
+            return False
+
+    # Vérification si le nombre apparait bien dans la colonne 
+    for i in range(0,9):
+        if tab[i][column] == number:
+            return False
+
+    # Vérification si le nombre donné qui apparait dans la case est un nombre ou pas
+    x0 = (column // 3) * 3
+    y0 = (row // 3) * 3
+    for i in range(0,3):
+        for j in range(0,3):
+            if tab[y0+i][x0+j] == number:
+                return False
+    return True
+
+def solve():
+    global tab
+    for row in range(0,9):
+        for column in range(0,9):
+            if tab[row][column] == 0:
+                for number in range(1,10):
+                    if possible(row, column, number):
+                        tab[row][column] = number
+                        solve()
+                        tab[row][column] = 0
+                return
+    print(np.matrix(tab))
+solve()
