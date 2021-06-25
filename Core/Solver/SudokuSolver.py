a4070f725d2e94c8329e99325d3b3279ea7b3c17
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-25 16:22:33 +0200

    Added master_solver

--- Core/Solver/SudokuSolver.py
+++ Core/Solver/SudokuSolver.py
@@ -6,6 +6,17 @@ class SudokuSolver(ABC):
         self.name = name
         self.sudoku = sudoku
 
+    def master_solver(self, solvers):
+        """
+        Solve the sudoku, with all the solvers provided, non-instanced
+        :type solvers: list[type[SudokuSolver]]
+        """
+        for solver in solvers:
+            solver(self.sudoku).solve()
+        for k, v in self.sudoku.sols:
+            if len(v) == 1:
+                self.sudoku.base[k] = list(v)[0]
+
     def get_poss(self):
         return set(range(1, 10))
 
