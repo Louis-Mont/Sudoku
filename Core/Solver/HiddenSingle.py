1507bc22a86d052b694b053948c69c78f5fb9057
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-25 15:23:18 +0200

    Added HiddenSingle.py

---
+++ Core/Solver/HiddenSingle.py
@@ -0,0 +1,57 @@
+from Core.Solver.SudokuSolver import SudokuSolver
+from Core.utils import sq_o, tryremove
+
+
+class HiddenSingle(SudokuSolver):
+    def __init__(self, sudoku):
+        super().__init__("Hidden Single", sudoku)
+
+    def solve(self):
+        sudoku = self.sudoku
+        for x, r in enumerate(sudoku.base):
+            for y in range(len(r)):
+                if sudoku.base[x, y] == 0:
+                    s = self.h_poss(x).union(self.v_poss(y)).union(self.sq_poss(x, y))
+                    if len(s) == 1:
+                        sudoku.base[x, y] = list(s)[0]
+
+    def h_poss(self, x):
+        all_poss = set(range(1, 10))
+        for y, c in enumerate(self.sudoku.base[x]):
+            try:
+                for s in self.sudoku.sols[x, y]:
+                    tryremove(all_poss, s)
+            except KeyError:
+                tryremove(all_poss, self.sudoku.base[x, y])
+        return all_poss
+
+    def v_poss(self, y):
+        all_poss = set(range(1, 10))
+        for x, r in enumerate(self.sudoku.base[:, y]):
+            try:
+                for s in self.sudoku.sols[x, y]:
+                    tryremove(all_poss, s)
+            except KeyError:
+                tryremove(all_poss, self.sudoku.base[x, y])
+        return all_poss
+
+    def sq_poss(self, x, y):
+        all_poss = set(range(1, 10))
+        x0 = sq_o(x)
+        y0 = sq_o(y)
+        for r, sq_x in enumerate(self.sudoku.base[x0:x0 + 3, y0:y0 + 3]):
+            for c, sq_y in enumerate(sq_x):
+                try:
+                    for v in self.sudoku.sols[x0 + r, y0 + c]:
+                        tryremove(all_poss, v)
+                except KeyError:
+                    tryremove(all_poss, self.sudoku.base[x0 + r, y0 + c])
+        return all_poss
+
+    def rm_poss(self, x, y, lp):
+        try:
+            for s in self.sudoku.sols[x, y]:
+                tryremove(lp, s)
+        except KeyError:
+            tryremove(lp, self.sudoku.base[x, y])
+        return lp
--- main.py
+++ main.py
@@ -2,19 +2,38 @@ import numpy as np
 import random
 from Core.Sudoku import Sudoku
 from Core.Solver.NakedSingle import NakedSingle
+from Core.Solver.HiddenSingle import HiddenSingle
 
 if __name__ == '__main__':
+    exhs = [[2, 0, 0, 0, 7, 0, 0, 3, 8],
+            [0, 0, 0, 0, 0, 6, 0, 7, 0],
+            [3, 0, 0, 0, 4, 0, 6, 0, 0],
+            [0, 0, 8, 0, 2, 0, 7, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 6],
+            [0, 0, 7, 0, 3, 0, 4, 0, 0],
+            [0, 0, 4, 0, 8, 0, 0, 0, 9],
+            [0, 6, 0, 4, 0, 0, 0, 0, 0],
+            [9, 1, 0, 0, 6, 0, 0, 0, 2]]
+
     grid = [[0 for i in range(9)] for j in range(0, 9)]
 
-    arr = np.array(grid)
+    arr = np.array(exhs)
     sdk = Sudoku(arr)
 
     sdk.generate_complete_sudoku()
     # print(sdk.base)
 
     sdk.drill_sudoku(40)
-    print(sdk.base)
+    sdkbase = sdk.base.copy()
+    print(sdkbase)
     print()
     ns = NakedSingle(sdk)
     ns.solve()
+    sdkns = sdk.base
+    print((sdkbase == sdkns).all())
+    hs = HiddenSingle(sdk)
+    hs.solve()
+    print()
+    print((sdkns == sdk.base).all())
+
     print(sdk.base)
