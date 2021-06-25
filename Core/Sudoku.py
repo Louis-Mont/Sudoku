7cf2914c47b193ed4911d4cc4be985bf9a0c6761
ER1 <erbreiz95@gmail.com>
2021-06-25 17:04:13 +0200

    Ajout vérification sudoku valide et creation de sudoku via texte (formattage via string)

--- Core/Sudoku.py
+++ Core/Sudoku.py
@@ -29,11 +29,84 @@ class Sudoku:
                     break
             x += 1
 
+    def generate_complete_sudoku2(self):
+        x = 0
+        while x < len(self.base):
+            for y in range(len(self.base[x])):
+                try:
+                    ns = NakedSingle(self)
+                    c_sol = ns.get_sol(x, y)
+                    pos = random.randint(0, len(c_sol) - 1)
+                    self.base[x, y] = list(c_sol)[pos]
+                except ValueError:
+                    self.base[x] = [0 for i in range(9)]
+                    x -= 1
+                    break
+            x += 1
+
+    def generate_complete_sudoku3(self):
+        for n in range(1, len(self.base)):
+            x = 0
+            while x < len(self.base):
+                y = 0
+                row_sols = []
+                while y < len(self.base[x]):
+                    if self.base[x,y] == 0:
+                        row_sols.append(y)
+                    y += 1
+                pos = random.randint(0, len(row_sols) - 1)
+
+                self.base[x, pos] = n
+                x += 1
+
     def drill_sudoku(self, c):
         i = 0
-        while i < c:
+        while i < c and not self.is_sudoku_empty():
             x = random.randint(0, len(self.base) - 1)
             y = random.randint(0, len(self.base[x]) - 1)
             if self.base[x, y] > 0:
                 self.base[x, y] = 0
                 i += 1
+
+    def is_sudoku_empty(self):
+        is_empty = True
+        for x in range(0, len(self.base)):
+            if not list(self.base[x]).count(0) == len(self.base[x]):
+                is_empty = False
+
+        return is_empty
+
+    def is_sudoku_valid(self):
+        is_valid = True
+        for x in range(len(self.base)):
+            for y in range(len(self.base[x])):
+                ns = NakedSingle(self)
+                val = self.base[x, y]
+                self.base[x, y] = 0
+                c_sol = ns.get_sol(x, y)
+                if val != 0 and not (list(c_sol).count(val) > 0):
+                    print(c_sol)
+                    print(val)
+                    is_valid = False
+                self.base[x, y] = val
+
+        return is_valid
+
+    def formating_sudoku(self, fc):
+        x = 0
+        y = 0
+        for car in fc:
+            if y == len(self.base[x]):
+                y = 0
+                x += 1
+            try:
+                c = int(car)
+                if c >= 0 and c <= 9:
+                    self.base[x,y] = c
+                    y += 1
+            except ValueError:
+                if y != len(self.base[x]) and x != len(self.base) and not str(car) == "\n":
+                    self.base[x,y] = 0
+                    y += 1
+                else:
+                    continue
--- main.py
+++ main.py
@@ -9,12 +9,23 @@ if __name__ == '__main__':
     arr = np.array(grid)
     sdk = Sudoku(arr)
 
-    sdk.generate_complete_sudoku()
-    # print(sdk.base)
+    #
+    sudoku_file = "908000605\n645793021\n000058000\n007130298\n039000010\n010070560\n350907006\n000310400\n490506072"
+    sudoku_file2 = "9-8---6-5\n645793-21\n----58---\n--713-298\n-39----1-\n-1--7-56-\n35-9-7--6\n---31-4--\n49-5-6-72"
+    sudoku_file3 = "9.8...6.5645793.21....58.....713.298.39....1..1..7.56.35.9.7..6...31.4..49.5.6.72"
+    sdk.formating_sudoku(sudoku_file)
+    #
 
-    sdk.drill_sudoku(40)
+    #sdk.generate_complete_sudoku()
     print(sdk.base)
+
+    #sdk.drill_sudoku(40)
+    #print(sdk.base)
+
+    is_valid = sdk.is_sudoku_valid()
+    print(is_valid)
+
     print()
     ns = NakedSingle(sdk)
     ns.solve()
-    print(sdk.base)
+    #print(sdk.base)
