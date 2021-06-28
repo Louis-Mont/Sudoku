6c0d711f238e12a37462207a84d6a7d67f60e6e9
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-28 23:17:03 +0200

    Corrected HiddenSingle.py, seems to work

--- Core/Solver/HiddenSingle.py
+++ Core/Solver/HiddenSingle.py
@@ -11,30 +11,36 @@ class HiddenSingle(SudokuSolver):
         for x, r in enumerate(sudoku.base):
             for y, c in enumerate(r):
                 if sudoku.base[x, y] == 0:
-                    s = self.h_poss(x, y).union(self.v_poss(x, y)).union(self.sq_poss(x, y))
-                    if len(s) == 1:
-                        sudoku.base[x, y] = list(s)[0]
+                    for s in [self.h_poss(x, y), self.v_poss(x, y), self.sq_poss(x, y)]:
+                        if len(s) == 1:
+                            self.sudoku.sols[x, y] = s
+                            break
 
     def h_poss(self, xb, yb):
-        all_poss = self.get_poss()
+        case_poss = self.sudoku.sols[xb, yb].copy()
         for y, c in enumerate(self.sudoku.base[xb]):
-            if yb != y:
-                self.rm_poss(xb, y, all_poss)
-        return all_poss
+            if yb != y and self.sudoku.base[xb, y] == 0:
+                case_poss -= self.sudoku.sols[xb, y]
+        return case_poss
 
     def v_poss(self, xb, yb):
-        all_poss = self.get_poss()
+        case_poss = self.sudoku.sols[xb, yb].copy()
         for x, r in enumerate(self.sudoku.base[:, yb]):
-            if xb != x:
-                self.rm_poss(x, yb, all_poss)
-        return all_poss
+            if xb != x and self.sudoku.base[x, yb] == 0:
+                case_poss -= self.sudoku.sols[x, yb]
+        return case_poss
 
     def sq_poss(self, xb, yb):
-        all_poss = self.get_poss()
+        case_poss = self.sudoku.sols[xb, yb].copy()
         x0 = sq_o(xb)
         y0 = sq_o(yb)
         for x, r in enumerate(self.sudoku.base[x0:x0 + 3, y0:y0 + 3]):
+            # real position of x in the global table
+            ax = x0 + x
             for y, c in enumerate(r):
-                if xb != x0 + x or yb != y0 + y:
-                    self.rm_poss(x0 + x, y0 + y, all_poss)
-        return all_poss
+                # real position of y in the global table
+                ay = y0 + y
+                # parentheses to change priority of operations
+                if (xb != ax or yb != ay) and self.sudoku.base[ax, ay] == 0:
+                    case_poss -= self.sudoku.sols[ax, ay]
+        return case_poss
