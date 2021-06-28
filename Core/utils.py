8dadebd2a87746a6f2c723064fe89f757c28a88b
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-28 14:32:20 +0200

    Added rm_coll

--- Core/utils.py
+++ Core/utils.py
@@ -11,3 +11,13 @@ def tryremove(c, v):
         # TODO : Not Good Sudoku
         pass
     return c
+
+
+def rm_coll(r_items, items):
+    """
+    :param r_items: the items you want to be removed from the list
+    :param items: the list where the items are removed
+    """
+    for s in r_items:
+        tryremove(items, s)
+    return items
