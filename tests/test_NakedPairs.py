244ee6f96654b4ae35a28538f5dffb94957a6d41
Louis-Mont <Louis-Mont@users.noreply.github.com>
2021-06-29 00:05:57 +0200

    Second test seems concluant

--- tests/test_NakedPairs.py
+++ tests/test_NakedPairs.py
@@ -11,15 +11,26 @@ class TestNakedPairs(TestCase):
     def __init__(self, *args, **kwargs):
         super(TestNakedPairs, self).__init__(*args, **kwargs)
         ex1 = [[4, 0, 0, 0, 0, 0, 9, 3, 8],
-                [0, 3, 2, 0, 9, 4, 1, 0, 0],
-                [0, 9, 5, 3, 0, 0, 2, 4, 0],
-                [3, 7, 0, 6, 0, 9, 0, 0, 4],
-                [5, 2, 9, 0, 0, 1, 6, 7, 3],
-                [6, 0, 4, 7, 0, 3, 0, 9, 0],
-                [9, 5, 7, 0, 0, 8, 3, 0, 0],
-                [0, 0, 3, 9, 0, 0, 4, 0, 0],
-                [2, 4, 0, 0, 3, 0, 7, 0, 9]]
+               [0, 3, 2, 0, 9, 4, 1, 0, 0],
+               [0, 9, 5, 3, 0, 0, 2, 4, 0],
+               [3, 7, 0, 6, 0, 9, 0, 0, 4],
+               [5, 2, 9, 0, 0, 1, 6, 7, 3],
+               [6, 0, 4, 7, 0, 3, 0, 9, 0],
+               [9, 5, 7, 0, 0, 8, 3, 0, 0],
+               [0, 0, 3, 9, 0, 0, 4, 0, 0],
+               [2, 4, 0, 0, 3, 0, 7, 0, 9]]
         self.ex1 = np.array(ex1)
+
+        ex2 = [[0, 8, 0, 0, 9, 0, 0, 3, 0],
+               [0, 3, 0, 0, 0, 0, 0, 6, 9],
+               [9, 0, 2, 0, 6, 3, 1, 5, 8],
+               [0, 1, 0, 8, 0, 4, 5, 9, 0],
+               [8, 5, 1, 9, 0, 7, 0, 4, 6],
+               [3, 9, 4, 6, 0, 5, 8, 7, 0],
+               [5, 6, 3, 0, 4, 0, 9, 8, 7],
+               [2, 0, 0, 0, 0, 0, 0, 1, 5],
+               [0, 1, 0, 0, 5, 0, 0, 2, 0]]
+        self.ex2 = np.array(ex2)
 
         self.solvers = [NakedSingle, HiddenSingle, NakedPairs]
 
@@ -33,3 +44,14 @@ class TestNakedPairs(TestCase):
 
         self.assertNotEqual(sol, sdk.sols)
         self.assertEqual({2, 5}, sdk.sols[0, 3])
+
+    def test_solve2(self):
+        sdk = Sudoku(self.ex2)
+        NakedSingle(sdk).solve()
+
+        sol = sdk.sols.copy()
+
+        master_solver(self.solvers, sdk)
+
+        self.assertNotEqual(sol, sdk.sols)
+        self.assertEqual({8, 9}, sdk.sols[7, 2])
